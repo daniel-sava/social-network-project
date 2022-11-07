5bbddfd656115c83978f2250c9ee3ce1f653cd51
Daniel Sava <daniel@bitcode.ro>
2022-11-07 20:36:10 +0200

    Add Part 2 of Social Network Project

--- scripts/index.js
+++ scripts/index.js
@@ -2,24 +2,45 @@ let networkUsers = [];
 let skills = {};
 let positions = {};
 
-const maxDisplayedUsers = 10;
+// active, blocked, idle, pending
+
+let currentUser = {};
+
+const maxDisplayedUsers = 7;
+let totalPages = 0;
+let currentPage = 2;
 
 const networkFeedElement = document.getElementById('network');
+const networkFeedResultsNumberElement =
+    document.getElementById('results-number');
+const networkFeedPaginationElement =
+    document.getElementById('network-pagination');
+const accountElement = document.getElementById('account');
 
 function getNetworkUsers() {
     let getNetworkUsersRequest = fetch(
-        'https://random-data-api.com/api/v2/users?size=100'
+        'https://random-data-api.com/api/v2/users?size=43'
     );
     getNetworkUsersRequest
         .then((response) => response.json())
         .then((data) => {
             networkUsers = data;
             let currentDisplayedUsers = 0;
+
+            // Preia primul utilizator, ca fiind utilizatorul curent
+            currentUser = new NetworkUser(networkUsers[0]);
+            createUserProfileElement(currentUser, accountElement);
+            // Sterge primul utilizator din lista cu toti utilizatorii
+            networkUsers = networkUsers.slice(1);
+            totalPages = Math.ceil(networkUsers.length / maxDisplayedUsers);
+
+            getPagesConfiguration();
+
             for (let networkUser of networkUsers) {
                 const user = new NetworkUser(networkUser);
 
                 if (currentDisplayedUsers !== maxDisplayedUsers) {
-                    createUserProfileElement(user);
+                    createUserProfileElement(user, networkFeedElement);
                     currentDisplayedUsers++;
                 }
 
@@ -28,6 +49,7 @@ function getNetworkUsers() {
             }
         })
         .catch((error) => {
+            console.log(error);
             throw new Error();
         });
 }
@@ -48,7 +70,11 @@ function addPositionToList(position) {
     }
 }
 
-function createUserProfileElement(user) {
+function createUserProfileElement(user, parentElement) {
+    if (!parentElement) {
+        return;
+    }
+
     const userProfileElement = document.createElement('div');
     userProfileElement.classList.add('user-profile');
     userProfileElement.id = `user-profile-${user.id}`;
@@ -56,6 +82,7 @@ function createUserProfileElement(user) {
     const userProfileAvatarElement = document.createElement('img');
     userProfileAvatarElement.classList.add('avatar');
     userProfileAvatarElement.src = user.personalData.avatar;
+    userProfileAvatarElement.loading = 'lazy';
 
     userProfileElement.appendChild(userProfileAvatarElement);
 
@@ -68,6 +95,7 @@ function createUserProfileElement(user) {
     const userFullName = document.createTextNode(
         `${user.personalData.firstName} ${user.personalData.lastName}`
     );
+
     userProfilePersonalDataName.appendChild(userFullName);
 
     const userProfilePersonalDataJobTitle = document.createElement('p');
@@ -81,7 +109,67 @@ function createUserProfileElement(user) {
 
     userProfileElement.appendChild(userProfilePersonalDataElement);
 
-    networkFeedElement.appendChild(userProfileElement);
+    const userPlanElement = document.createElement('div');
+    userPlanElement.classList.add(
+        'subscription-plan',
+        user.subscription.status.toLowerCase()
+    );
+
+    const userPlanText = document.createTextNode(user.subscription.plan);
+
+    userPlanElement.appendChild(userPlanText);
+
+    userProfileElement.appendChild(userPlanElement);
+
+    if (parentElement === accountElement) {
+        const userNameElement = document.createElement('span');
+        const userNameText = document.createTextNode(
+            ` @${user.personalData.username}`
+        );
+
+        userNameElement.appendChild(userNameText);
+
+        userProfilePersonalDataName.appendChild(userNameElement);
+
+        const userBioElement = document.createElement('p');
+        userBioElement.classList.add('bio');
+
+        const userBioText = document.createTextNode(
+            `I am a ${user.employment.mainSkill} ${user.employment.position} from ${user.address.city}, ${user.address.country}.`
+        );
+
+        userBioElement.appendChild(userBioText);
+        userProfilePersonalDataElement.appendChild(userBioElement);
+    }
+
+    parentElement.appendChild(userProfileElement);
+}
+
+function getPagesConfiguration() {
+    for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
+        const pageNumberElement = document.createElement('div');
+        pageNumberElement.classList.add('page-number');
+
+        const pageNumberText = document.createTextNode(pageNumber);
+        pageNumberElement.appendChild(pageNumberText);
+
+        networkFeedPaginationElement.appendChild(pageNumberElement);
+    }
+
+    setResultsNumberElement();
+}
+
+// max displayed = 10
+// current page = 2 => Showing users from 11 to 20
+
+function setResultsNumberElement() {
+    const startingNumber = (currentPage - 1) * maxDisplayedUsers + 1;
+    const endingNumber =
+        currentPage === totalPages
+            ? networkUsers.length
+            : currentPage * maxDisplayedUsers;
+
+    networkFeedResultsNumberElement.textContent = `Showing users from ${startingNumber} to ${endingNumber} out of ${networkUsers.length}`;
 }
 
 getNetworkUsers();
@@ -91,6 +179,7 @@ class NetworkUser {
     address;
     personalData;
     employment;
+    subscription;
 
     constructor(data) {
         this.id = data.id;
@@ -98,6 +187,7 @@ class NetworkUser {
         this.setUserAddress(data.address);
         this.setUserPersonalData(data);
         this.setUserEmploymentData(data.employment);
+        this.setUserSubscriptionData(data.subscription);
     }
 
     setUserAddress(address) {
@@ -124,6 +214,13 @@ class NetworkUser {
         this.employment = {
             position: employmentData.title,
             mainSkill: employmentData.key_skill,
+        };
+    }
+
+    setUserSubscriptionData(subscriptionData) {
+        this.subscription = {
+            plan: subscriptionData.plan,
+            status: subscriptionData.status,
         };
     }
 }
